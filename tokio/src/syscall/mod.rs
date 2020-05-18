87afd149126554567a103b7db06cebb3bc0a8e1e
Gardner Vickers <gardner@vickers.me>
2020-05-17 22:42:13 -0700

    (syscalls) Initial implementation of Syscalls
    - Changes JoinHandle to allow wrapping a future
      as well as a RawTask.
    - Add config macros for enabling syscall related
      functionality.
    - Add `Runtime` variants for dispatching to a `Syscalls`
      based runtime.

---
+++ tokio/src/syscall/mod.rs
@@ -0,0 +1,39 @@
+//! The [syscall] module is intended to provide a centralized location
+//! for interacting with OS resources such as disks and network.
+//!
+//! This requires both the `"test-util"` feature to be enabled as well
+//! as the `--cfg tokio_unstable` flag to be supplied.
+//!
+//! ## Extension
+//! The Syscall trait allows hooking into implementations of Tokio
+//! disk and networking resources to supply alternate implementations
+//! or mocks.
+//!
+//! Extension requires compiling with `--cfg tokio_unstable` in addition
+//! to the `syscall` feature flag.
+//!
+//! [syscall]:crate::syscall
+mod default;
+pub(crate) use default::DefaultSyscalls;
+use std::future::Future;
+
+use std::io;
+use std::pin::Pin;
+
+/// Syscalls trait allows for hooking into the Tokio runtime.
+pub trait Syscalls: Send + Sync {
+    /// Spawn a Future onto the runtime.
+    fn spawn(&self, future: Pin<Box<dyn Future<Output = ()>>>);
+
+    /// Spawn a blocking task onto the runtime.
+    fn spawn_blocking(&self, task: Box<dyn FnOnce()>);
+
+    /// Drive the runtime forward
+    fn park(&self) -> Result<(), io::Error>;
+
+    /// Drive the runtime forward with a timeout.
+    fn park_timeout(&self, duration: std::time::Duration) -> Result<(), io::Error>;
+
+    /// Unblock the runtime
+    fn unpark(&self);
+}
